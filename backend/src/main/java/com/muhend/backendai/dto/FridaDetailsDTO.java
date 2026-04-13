dbfc6f1bf6a02cf7c79ab3c508e0d83b785bad2b
Muhend <hamohand@gmail.com>
2026-04-13 03:12:07 +0200

    Liste fridas

--- backend/src/main/java/com/muhend/backendai/dto/FridaDetailsDTO.java
+++ backend/src/main/java/com/muhend/backendai/dto/FridaDetailsDTO.java
@@ -12,5 +12,6 @@ public class FridaDetailsDTO {
     private LocalDate dateCreation; // frida
     private LocalDate dateNaissance; // extrait de naissance du défunt
     private String nom; // extrait de naissance du défunt
+    private String prenom; // prenom du défunt
     private Boolean requiresCorrection;
 }
