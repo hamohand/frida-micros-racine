3ac60768abcfa69ca5a41378a4969daef2c50a4c
Muhend <hamohand@gmail.com>
2026-06-17 05:25:07 +0200

    fix(dashboard): configure OAuth Interceptor and allowedUrls

--- license-dashboard/src/app/app.config.ts
+++ license-dashboard/src/app/app.config.ts
@@ -1,17 +1,17 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { OAuthModule } from 'angular-oauth2-oidc';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
     importProvidersFrom(OAuthModule.forRoot({
       resourceServer: {
-        allowedUrls: ['https://licences.frida.enclume-numerique.com/api'],
+        allowedUrls: ['/api', 'https://licences.frida.enclume-numerique.com/api'],
         sendAccessToken: true
       }
     }))
